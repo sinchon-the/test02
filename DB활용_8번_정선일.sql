df2ea9c671a076e6cbcaba3f861001d99636dd5b
cooljsi <65692912+cooljsi@users.noreply.github.com>
2020-06-15 17:46:03 +0900

    Add files via upload

---
+++ DB활용_8번_정선일.sql
@@ -0,0 +1,20 @@
+CREATE TABLE TEST 
+(
+  NO NUMBER NOT NULL PRIMARY KEY
+, NAME VARCHAR2(20) 
+, SAL NUMBER 
+
+);
+
+desc test;
+
+insert into test VALUES(1, 차승원, 2000);
+insert into test(no, name, sal) VALUES(2, 유해진,2500);
+insert into test(no, name, sal) VALUES(3, 손호준, 1500);
+select * from test;
+update test SET sal=2500 where no=1;
+select * from test where sal < 2000;
+update test set sal=3000 where sal>=2000;
+select * from test;
+delete from test;
+commit;
